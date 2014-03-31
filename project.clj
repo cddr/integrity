7e0fc351e67f787094dce949ecf25e53721bfd4a
Andy Chambers <anchambers@paypal.com>
2014-03-30 23:11:57 -0400

    Fix description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject schema.contrib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A collection of namespaces that operate on prismatic/schema
+metadata and functions"
+  :url "https://github.com/cddr/schema.contrib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
