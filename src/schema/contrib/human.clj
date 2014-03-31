5bf2ef1099ffd54f05ad33ac0d438c7264741f79
Andy Chambers <anchambers@paypal.com>
2014-03-30 23:09:41 -0400

    Refactor human-explain to use ValidationTranslator protocol

--- src/schema/contrib/human.clj
+++ src/schema/contrib/human.clj
@@ -1,43 +1,61 @@
 (ns schema.contrib.human
   (:require [schema.core :as s]
-            [schema.utils :as utils]))            
+            [schema.utils :as utils]))
 
-(defn human-explain
-  ([err]
-     (when err
-       (human-explain err true)))
-  ([err top-level?]
-     (let [err* {:explain (utils/.-fail-explanation err)
-                 :schema (utils/.-schema err)
-                 :value (utils/.-value err)
-                 :expectation @(utils/.-expectation-delay err)
-                 :fail-explanation (utils/.-fail-explanation err)}
-           humanize (fn [v]
-                      (if (symbol? v)
-                        (str (name v))
-                        v))
-           show-val (fn []
-                      (if top-level?
-                        (:value err*)
-                        "it"))]
-       (cond
-        (instance? schema.core.Predicate (:schema err*))
-        (with-out-str
-          (print (show-val) "is not ")
-          (apply print (let [[op & args] (first (:expectation err*))]
-                         (conj (interpose "and" (map humanize args))
-                               (humanize op)))))
+(defn- humanize [v]
+  (if (symbol? v)
+    (str (name v))
+    v))
 
-        (instance? schema.core.Either (:schema err*))
-        (with-out-str
-          (println (show-val) "fails all of the following:-")
-          (doseq [exp (map #(human-explain % nil)
-                           (map #(s/check % (:value err*))
-                                (.-schemas (:schema err*))))]
-            (println " " exp)))
+(defn- show-val [err parent]
+  (if (nil? parent)
+    (:value err)
+    "it"))
 
-        true
-        (with-out-str
-          (print (show-val) "is not a" (:schema err*)))))))
+(defn- error [check-result]
+  {:explain (utils/.-fail-explanation check-result)
+   :schema (utils/.-schema check-result)
+   :value (utils/.-value check-result)
+   :expectation @(utils/.-expectation-delay check-result)
+   :fail-explanation (utils/.-fail-explanation check-result)})
 
+(defprotocol ValidationTranslator
+  (translate [schema error parent]))
+
+(extend schema.core.Predicate
+  ValidationTranslator
+  {:translate (fn [schema error parent]
+                (with-out-str
+                  (print (show-val error parent) "is not ")
+                  (apply print (let [[op & args] (first (:expectation error))]
+                                 (conj (interpose "and" (map humanize args))
+                                       (humanize op))))))})
+
+(extend schema.core.Either
+ ValidationTranslator
+ {:translate (fn [schema e p]
+               (with-out-str
+                 (println (show-val e p) "fails all of the following:-")
+                 (doseq [exp (map (fn [sub-schema]
+                                    (let [val (:value e)
+                                          check-result (s/check sub-schema val)]
+                                      (if check-result
+                                        (translate (:schema (error check-result))
+                                                   (error check-result)
+                                                   e))))
+                                  (.-schemas schema))]
+                   (println " " exp))))})
+
+
+
+(extend java.lang.Class
+  ValidationTranslator
+  {:translate (fn [schema error parent]
+                (with-out-str
+                  (print (show-val error parent) "is not a" (:schema error))))})
+
+(defn human-explain [check-result]
+  (when check-result
+    (let [error (error check-result)]
+      (translate (:schema error) error nil))))
 
